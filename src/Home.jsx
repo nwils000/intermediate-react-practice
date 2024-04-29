804ec259c3f2bb2641b3c34185f4c91769589203
nwils000 <nwils000@gmail.com>
2024-04-29 17:57:57 -0400

    finished the assignment

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,31 +2,28 @@ import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { TeamContext } from './main';
 
-const Title = () => {
-  return <h1>Hello World!</h1>;
-};
-
 function Home() {
   const { state } = useContext(TeamContext);
   let players = [];
 
   for (let player of state.team) {
     players.push(
-      <ul>
-        <li>Name: {player.name}</li>
-        <li>Health: {player.health}</li>
-        <li>Attack: {player.attack}</li>
-        <li>Speed: {player.speed}</li>
-      </ul>
+      <div className="ul-wrapper">
+        <li className="player-name">{player.name}</li>
+        <ul className="inner-list">
+          <li>Health: {player.health}</li>
+          <li>Attack: {player.attack}</li>
+          <li>Speed: {player.speed}</li>
+        </ul>
+      </div>
     );
   }
 
   return (
-    <div className='home'>
+    <div className="home">
       <Link to="/edit-team">Edit Team</Link>
       <h1>Players</h1>
       <ul>{players.map((e) => e)}</ul>
-      <Title />
     </div>
   );
 }
